67fbadbf462b173279599d8d56ee6864b520f88e
Mehmet Ali Ayık <mehmet.ayik@digilera.com>
2025-01-19 19:05:47 +0300

    2 service  communication

--- Microservice1.API/Controllers/ProductsController.cs
+++ Microservice1.API/Controllers/ProductsController.cs
@@ -9,10 +9,12 @@ namespace Microservice1.API.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly AppDbContext _context;
+        private readonly IConfiguration _configuration;
 
-        public ProductsController(AppDbContext context)
+        public ProductsController(AppDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         [HttpPost]
@@ -21,6 +23,18 @@ namespace Microservice1.API.Controllers
             _context.Products.Add(new Product { Name = "Product 1"});
             _context.SaveChanges();
             return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync($"{_configuration.GetSection("Microservices")["baseUrlMicroservice2"]}/api/products");
+
+            var context = response.Content.ReadAsStringAsync();
+            
+            return Ok(context);
         }
     }
 }
---
+++ Microservice2.API/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservice2.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Id = 1, name = "Product 1(Microservice2)"});
+        }
+    }
+}
